e6774c8802845ba7ae56859d67414658c9d9f921
Luis Fernando <luissierradelgado6@gmail.com>
2025-07-22 16:31:39 -0600

    Se realizaron los endpoints en las rutas, asi como las diferentes acciones pertenecientes en joyas controller (eliminar, buscar, actualizar, agregar)

--- app/controllers/joyasController.js
+++ app/controllers/joyasController.js
@@ -3,15 +3,97 @@ const joyasModel = require('../models/joyasModel')
 function buscarTodo(req, res) {
     joyasModel.find({})
     .then(joyas => {
-        if (joyas.lenght) {
-            return res.status(200).send({joyas})
+        if (joyas.length) {
+            return res.status(200).send({joyas}) 
         }
-        return res.status(204).send({menaje: "No hay nada que mostrar"})
+        return res.status(204).send({menaje: "No hay nada que mostrar"}) // Se manda pero bo hay información
     })
     .catch(e => {return res.status(404).send({mensaje: `Eror al consultar la información ${e}`})})
 }
 
-module.exports = {
-    buscarTodo
-}
+function agregarJoya(req, res) {
     
+    new joyasModel(req.body).save()
+    .then(info => {
+        return res.status(200).send({
+            mensaje: "La información se guardo de forma correcta",
+            info
+        })
+    })
+    .catch(e => {return res.status(404).send({
+        mensaje:`error al guardar ${e}`
+    })})
+}
+
+function buscarJoya(req, res, next) {
+    if (!req.body) req.body = {}
+    var consulta = {}
+    consulta[req.params.key] = req.params.value
+    
+    joyasModel.find(consulta)
+    .then(joyas => {
+        if (!joyas.length) return next();
+        req.body.joyas = joyas
+        return next()
+    })
+    .catch(e => {
+        req.body.e = e
+        return next()
+    })
+}
+
+function eliminarJoya(req, res) {
+    var joyas = {}
+    joyas = req.body.joyas
+
+    joyasModel.deleteOne(joyas[0])
+    .then(info =>{
+        return res.status(200).send({mensaje: "La información se elimino de forma correcta", info})
+    })
+
+    .catch(e => {
+        return res.status(404).send({mensaje: "error al eliminar información", e})
+    })
+
+}
+
+function actualizarJoya(req, res) {
+    var joyas = {}
+    
+    if (!joyas || joyas.length) {
+        return res.status(200).send({
+            mensaje: "No se encontró la joya modificada",
+            info
+        })
+    }
+
+    joyasModel.updateOne(joyas[0], req.body)
+    .then(info => {
+        return res.status(200).send({
+            mensaje: "La información se actualizó correctamente",
+            info
+        })
+    })
+    .catch(e => {
+        return res.status(404).send({
+            mensaje: "Error al actualizar la información", e
+        })
+    })
+}
+
+
+function mostrarJoya(req, res) {
+    if(req.body.e){return res.status(404).send({mensaje: `error al buscar la información`})}
+    if(!req.body.joyas){return res.status(204).send({mensaje: "No hay nada que mostrar"})}
+    let joyas = req.body.joyas
+    return res.status(200).send({joyas})
+}
+
+module.exports = {
+    buscarTodo,
+    agregarJoya,
+    buscarJoya,
+    mostrarJoya,
+    eliminarJoya,
+    actualizarJoya
+}
--- app/routes/joyasRoute.js
+++ app/routes/joyasRoute.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const joyasController = require('../controllers/joyasController')
 
-router.get('/}', joyasController.buscarTodo)
+router.get('/', joyasController.buscarTodo)
+    .post('/', joyasController.agregarJoya)
+    .get('/:key/:value', joyasController.buscarJoya, joyasController.mostrarJoya)
+    .delete('/:key/:value', joyasController.buscarJoya, joyasController.eliminarJoya)
+    .put("/:key/:value", joyasController.buscarJoya, joyasController.actualizarJoya);
 
 module.exports = router
